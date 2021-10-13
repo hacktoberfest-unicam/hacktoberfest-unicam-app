324c756b38e587c9909d0ba91d0c576af6201d5e
Dmitry Mingazov <mingazov@email.com>
2021-10-13 17:39:10 +0200

    Add navbar barebones

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,9 @@
-export interface User {
+export interface User extends UserDTO{
     // Github username
+    closedPRs?: number[];
+}
+
+export interface UserDTO {
     nickname: string;
     name: string;
     surname: string;
