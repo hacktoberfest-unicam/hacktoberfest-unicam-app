3bf1a2c9a05b5c690da6050f230c76129fa25d63
Dmitry Mingazov <mingazov@email.com>
2021-10-19 09:25:22 +0200

    Move pull request logic to pr page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -14,56 +14,7 @@ import { PullRequestService } from '../services/pull-request.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-  public pullRequests: PullRequest[] = [];
-  public problems: Problem[] = [];
-  public users: User[] = [];
-
-  public expandedElement: string = '';
-
-  public displayedColumns = ['id', 'problemId', 'nickname', 'mergeTime', 'bonusPoints', 'bonusComment','actions'];
-
-  @ViewChild('prTable') prTable: MatTable<PullRequest>;
-
-  constructor(
-    private pullRequestService: PullRequestService,
-    private dialog: MatDialog
-    ) { }
-
-  ngOnInit(): void {
-    let pr$ = environment.debug ? this.pullRequestService.getPullRequestsMOCK() : this.pullRequestService.getPullRequests();
-    pr$.subscribe(prs => {
-      this.pullRequests = prs;
-    });
-  }
-
-  public editPullRequest(pullRequestId: number): void {
-    let tmp = this.dialog.open(EditPullRequestDialogComponent, {
-      data: this.pullRequests.find(pr => pullRequestId === pr.id)
-    }).afterClosed().subscribe(value => {
-      if (!value) {
-        return;
-      }
-      switch(value.command) {
-        case Command.UPDATE:
-          let obsUpdate$ = environment.debug ? this.pullRequestService.updatePullRequestMOCK(value.pr) : this.pullRequestService.updatePullRequest(value.pr);
-          obsUpdate$.subscribe(pr => {
-            let index = this.pullRequests.findIndex(el => el.id === pr.id);
-            this.pullRequests.splice(index,1);
-            this.pullRequests.push(pr);
-          });
-          break;
-        case Command.DELETE:
-          let obsDelete$ = environment.debug ? this.pullRequestService.deletePullRequestMOCK(value.pr) : this.pullRequestService.deletePullRequest(value.pr);
-          obsDelete$.subscribe(pr => {
-            let index = this.pullRequests.findIndex(el => el.id === value.pr.id);
-            this.pullRequests.splice(index,1);
-          });
-          break;
-        default:
-      }
-      this.prTable.renderRows();
-    });
-  }
+export class DashboardComponent {
+    constructor() { }
 
 }
--- src/app/pull-request/pull-request.component.ts
+++ src/app/pull-request/pull-request.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTable } from '@angular/material/table';
+import { environment } from 'src/environments/environment';
+import { Command } from '../dialogs/command.enum';
+import { EditPullRequestDialogComponent } from '../dialogs/edit-pull-request-dialog.component';
+import { Problem } from '../models/problem';
+import { PullRequest } from '../models/pull-request';
+import { User } from '../models/user';
+import { PullRequestService } from '../services/pull-request.service';
 
 @Component({
   selector: 'app-pull-request',
@@ -6,10 +15,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pull-request.component.scss']
 })
 export class PullRequestComponent implements OnInit {
+  public pullRequests: PullRequest[] = [];
+  public problems: Problem[] = [];
+  public users: User[] = [];
 
-  constructor() { }
+  public expandedElement: string = '';
+
+  public displayedColumns = ['id', 'problemId', 'nickname', 'mergeTime', 'bonusPoints', 'bonusComment','actions'];
+
+  @ViewChild('prTable') prTable: MatTable<PullRequest>;
+
+  constructor(
+    private pullRequestService: PullRequestService,
+    private dialog: MatDialog
+    ) { }
 
   ngOnInit(): void {
+    let pr$ = environment.debug ? this.pullRequestService.getPullRequestsMOCK() : this.pullRequestService.getPullRequests();
+    pr$.subscribe(prs => {
+      this.pullRequests = prs;
+    });
   }
 
+  public editPullRequest(pullRequestId: number): void {
+    let tmp = this.dialog.open(EditPullRequestDialogComponent, {
+      data: this.pullRequests.find(pr => pullRequestId === pr.id)
+    }).afterClosed().subscribe(value => {
+      if (!value) {
+        return;
+      }
+      switch(value.command) {
+        case Command.UPDATE:
+          let obsUpdate$ = environment.debug ? this.pullRequestService.updatePullRequestMOCK(value.pr) : this.pullRequestService.updatePullRequest(value.pr);
+          obsUpdate$.subscribe(pr => {
+            let index = this.pullRequests.findIndex(el => el.id === pr.id);
+            this.pullRequests.splice(index,1);
+            this.pullRequests.push(pr);
+          });
+          break;
+        case Command.DELETE:
+          let obsDelete$ = environment.debug ? this.pullRequestService.deletePullRequestMOCK(value.pr) : this.pullRequestService.deletePullRequest(value.pr);
+          obsDelete$.subscribe(pr => {
+            let index = this.pullRequests.findIndex(el => el.id === value.pr.id);
+            this.pullRequests.splice(index,1);
+          });
+          break;
+        default:
+      }
+      this.prTable.renderRows();
+    });
+  }
 }
